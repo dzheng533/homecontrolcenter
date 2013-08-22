07777e4c2e550f940d685e91d04c12a704802d88
Vincent Dong <dzheng533@gmail.com>
2013-08-22 18:16:49 +0000

    Change file.

--- bmp085/bmpmain.c
+++ bmp085/bmpmain.c
@@ -6,7 +6,7 @@ int main(){
 int temperature,pressure = 0;
  if(!initBMP085(PRESSLEVEL)){
    printf("error.");
- } 
+ }
  convertBMP085All();
  readBMP085(&temperature,&pressure);
  printf("BMP085 Temperture: %1f ℃,Pressure:%d par\n",(double)temperature/10,pressure);
