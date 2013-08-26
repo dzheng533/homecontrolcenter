e55d3b0dcd3f9ee59f42ec8aaf3d9afa7f7d4052
Vincent Dong <dzheng533@gmail.com>
2013-08-26 16:29:05 +0000

    add RC-522 Read/Write functions.

--- rc522/rc522.c
+++ rc522/rc522.c
@@ -1,7 +1,21 @@
 #include "../common.h"
 #include <wiringPiSPI.h>
 #define CMDREGADD 0x01
+#define uchar unsigned char
 unsigned char cmdReg[2] = {0x01,0x2C};
+
+uchar readRc522Reg(uchar cmd){
+    uchar cmdREG = cmd;
+    wiringPiSPIDataRW(0,&cmdREG,1);
+    return cmdREG;
+}
+
+void writeRc522Reg(uchar cmd,uchar value){
+    uchar cmds[2]= {0x00,0x00};
+    cmds[0] = cmd;
+    cmds[1] = value;
+    wiringPiSPIDataRW(0,cmds,2);
+}
 
 int main(){
    if( wiringPiSetup() < 0 ){
@@ -11,11 +25,13 @@ int main(){
      return -2;
    }
    printf("Commang REG:");
-   printf("W:0x%02x; ",cmdReg[1]);
-   wiringPiSPIDataRW(0, cmdReg,2);
-   printf("AW:0x%02x; ",cmdReg[0]);
-   wiringPiSPIDataRW(0, cmdReg,1);
-   printf("R:0x%02x; ",cmdReg[0]);
+   //wiringPiSPIDataRW(0, cmdReg,1);
+   printf("R:0x%02x; ",readRc522Reg(CMDREGADD));
+   printf("BW:0x%02x; ",cmdReg[1]);
+   //cmdReg[0] = 0x01;
+   //wiringPiSPIDataRW(0, cmdReg,2);
+   writeRc522Reg(CMDREGADD,0x20);
+   //printf("AW:0x%02x; \n",cmdReg[0]);
 
    printf("Final:0x%02x,0x%02x",cmdReg[0],cmdReg[1]);
    printf("\n");
