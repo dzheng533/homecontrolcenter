ffd24b30baf5f1948991e99c4db93a7eaadec2af
vincent <dzheng533@gmail.com>
2013-09-08 15:12:16 +0000

    complete nrf905 send and receive

--- fork/fork.c
+++ fork/fork.c
@@ -1,22 +1,22 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <syslog.h>
-#include <sys/types.h> 
-#include <sys/wait.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define PIDFILE "/var/run/daemon-test.pid"
 
-int init_daemon() 
-{ 
-	  FILE *fp = NULL;
+int init_daemon()
+{
+    FILE *fp = NULL;
     pid_t fid;
     pid_t pid;
     int i,fd;
     char spid[10] = {'\0'};
-    
+
     //check lock file
     if(access(PIDFILE,F_OK)==0 && access(PIDFILE,R_OK)==0){
     	fp = fopen(PIDFILE,"rb+");
@@ -27,7 +27,7 @@ int init_daemon()
     }
 
     fid = fork();
-    if(fid < 0){ // 
+    if(fid < 0){ //
         printf("Fork Error!");
         return(-2);
     }
@@ -39,7 +39,6 @@ int init_daemon()
 		   for (i = 0;i < getdtablesize();i++){
 		     close(i);
 		   }
-		   
 		   chdir("/");
 		   umask(0);
     	 pid = getpid();
@@ -50,36 +49,32 @@ int init_daemon()
     	 fp = NULL;
     	 return 0;
     }
-} 
+}
 
-void sig_term(int signo)   
-{   
-    if(signo == SIGTERM)   
-/* catched signal sent by kill(1) command */   
-  {   
-    syslog(LOG_INFO, "program terminated.");   
+void sig_term(int signo){
+    if(signo == SIGTERM){
+    /* catched signal sent by kill(1) command */
+    syslog(LOG_INFO, "program terminated.");
     closelog();
     unlink(PIDFILE);
-    exit(0);   
-  }   
-}  
+    exit(0);
+  }
+}
 
-int main(int argc,char** args){ 
+int main(int argc,char** args){
 
   if(argc >1){
     if(args[1]
   }
-    if(init_daemon()){
-       
-       exit(-1);
-    }
-    
-    openlog("daemontest", LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "program started.");   
-    signal(SIGTERM, sig_term); /* arrange to catch the signal */  
+  if(init_daemon()){
+    exit(-1);
+  }
 
-    while(1){
-       sleep(10);
-    }
-    //exit(0); 
+  openlog("daemontest", LOG_PID, LOG_USER);
+  syslog(LOG_INFO, "program started.");
+  signal(SIGTERM, sig_term); /* arrange to catch the signal */
+
+  while(1){
+     sleep(10);
+  }
 }
--- nrf905/nrf905.c
+++ nrf905/nrf905.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
+
+#define PIN_TRXCE 2 //GPIO2
+#define PIN_AM    3 //GPIO3
+#define PIN_TXEN  1 //GPIO1
+#define PIN_PWR   4 //GPIO4
+#define PIN_CO    5 //GPIO5
+#define PIN_DR    6 //GPIO6
 
 #define WC 0x00
 #define RC 0x10
@@ -10,42 +18,190 @@
 #define RTXADD 0x23
 #define RRXBUF 0x24
 
-#define DEVADD 0xCC,0xCC,0xCC,0xCC
+//#define LOCADD    0xCC,0xCC,0xCC,0xCC
+//#define REMOTEADD 0xCC,0xCC,0xCC,0xCE
+
+#define LOCADD    0xCC,0xCC,0xCC,0xCE
+#define REMOTEADD 0xCC,0xCC,0xCC,0xCC
+
+#define DEVICEWORKMODE_STANDBY  0
+#define DEVICEWORKMODE_RECEIVE  1
+#define DEVICEWORKMODE_SEND     2
 
 #define uchar unsigned char
-//uchar rfConf[11] = {0x21,0x6c,0x00,0x44,0x20,0x20,0xe7,0xe7,0xe7,0xe7,0xe7};
 
-//read
-//uchar rfConf[11] = {0x10,0x4c,0x0c,0x44,0x04,0x04,0xcc,0xcc,0xcc,0xcc,0x58};
-int main(){
-  int i =0;
+uchar rfSConf[11] = {WC,
+                  0x4c,0x08,  //430.0 MHz +6db
+                  0x44,       //4 Bytes for TX and RX Address
+                  0x20,0x20,  //32 Bytes for TX and RX buffer
+                  LOCADD,     //Device Address
+                  0x58};      //8bit CRC EN 16Mhz, no CLK output
 
-  uchar rfSConf[11] = {WC,0x4c,0x04,0x44,0x20,0x20,DEVADD,0x58};
-  uchar rfRConf[11] = {RC,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-  uchar rxBuf[33] = {0x00};
-  uchar txAdd[5] = {0x00};
+uchar dataBuf[33] = {0x00};  //Data buffer 1 byte cmd, 32 bytes data
+uchar txAdd[5] = {WTXADD,REMOTEADD};   //Address buffer
 
+int writeCfg(uchar * rfCfg){
+  int i,sizeReg = 0;
+  sizeReg = sizeof(rfCfg);
   printf("Set Config:  ");
+  printf("Size:%d\n",sizeReg);
   for(i=0;i<11;i++){
-    printf("0x%lX ",rfSConf[i]);
+    printf("0x%02X ",rfCfg[i]);
   }
   printf ("\n");
+  //Write config
+  wiringPiSPIDataRW(0,rfSConf,11);
+}
+
+
+int initNRF905(){
+  //initinal as receive device
+  //Hardware init.
+  pinMode(PIN_PWR,OUTPUT);
+  pinMode(PIN_TXEN,OUTPUT);
+  pinMode(PIN_TRXCE,OUTPUT);
+  pinMode(PIN_CO,INPUT);
+  pinMode(PIN_AM,INPUT);
+  pinMode(PIN_DR,INPUT);
+  //Power off mode
+  digitalWrite(PIN_PWR,LOW);
+  digitalWrite(PIN_TRXCE,LOW);
+  digitalWrite(PIN_TXEN,LOW);
+  // init config
+  writeCfg(rfSConf);
+}
+int nrfMode(int mode){
+  if(mode == DEVICEWORKMODE_RECEIVE){
+    digitalWrite(PIN_PWR,HIGH);
+    digitalWrite(PIN_TRXCE,HIGH);
+    digitalWrite(PIN_TXEN,LOW);
+    delay(1);
+}else if(mode == DEVICEWORKMODE_SEND){
+    digitalWrite(PIN_PWR,HIGH);
+    digitalWrite(PIN_TRXCE,HIGH);
+    digitalWrite(PIN_TXEN,HIGH);
+  }else{
+    //Standby Mode
+    digitalWrite(PIN_PWR,HIGH);
+    digitalWrite(PIN_TRXCE,LOW);
+    digitalWrite(PIN_TXEN,LOW);
+  }
+}
+
+int send(uchar * addr,uchar * data){
+   //write remote address
+   int i = 0;
+   uchar txAddress[5] = {0x00};
+   uchar txData[33] = {0x00};
+
+   nrfMode(DEVICEWORKMODE_STANDBY);
+   delay(1);
+   printf("Address:");
+   for(i = 0;i<5;i++){
+      txAddress[i] = txAdd[i];
+      //printf("0x%02x ",txAdd[i]);
+   }
+   for(i = 0;i<5;i++){
+      printf("0x%02x ",txAddress[i]);
+   }
+   printf("\n");
+   txData[0] = 0x20;
+
+   txData[1] = 0x33;
+
+   for(i=1;i<33;i++){
+     txData[i] = data[i-1];
+   }
+
+   wiringPiSPIDataRW(0,txAddress,5);
+   //write data to buffer
+   printf("Data:");
+   for(i=0;i<33;i++)
+      printf("0x%02x ",txData[i]);
+   printf("\n");
+  // data
+   wiringPiSPIDataRW(0,txData,33);
+   //change device to send mode
+   nrfMode(DEVICEWORKMODE_SEND);
+   //while(digitalRead(PIN_DR)){
+   //  delay(1);
+   //}
+   return 0;
+}
+int dataAvailable(){
+   return digitalRead(PIN_DR);
+}
+int receive(uchar * data){
+   int i = 0;
+   uchar rxData[33] = {0x00};
+   rxData[0] = RRXBUF;
+   nrfMode(DEVICEWORKMODE_STANDBY);
+   wiringPiSPIDataRW(0,rxData,33);
+   nrfMode(DEVICEWORKMODE_RECEIVE);
+
+   //printf("Receive Data:");
+   for(i=0;i<33;i++){
+     //printf("0x%02X ",data[i]);
+     data[i] = rxData[i+1];
+   }
+   printf("\n");
+   return 0;
+}
+
+int main(int argc,char **args){
+  int i =0;
+  int mode= 1; //receive 1, send 2
+  int index = 1;
+  //uchar rfSConf[11] = {WC,0x4c,0x04,0x44,0x20,0x20,DEVADD,0x58};
+  uchar rfRConf[11] = {RC,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 
   wiringPiSetup();
   if(wiringPiSPISetup(0,500000) == -1){
-  	printf("Could not initialise SPI.\n");
+    printf("Could not initialise SPI.\n");
     return -1;
   }
-
+  initNRF905();
+  //writeCfg(rfSConf);
   //Write config
   //wiringPiSPIDataRW(0,rfSConf,11);
 
   //Read config
   wiringPiSPIDataRW(0,rfRConf,11);
-  printf("Read Config: ");
+  printf("Read Config: \n");
   for(i=0;i<11;i++){
-    printf("0x%X ",rfRConf[i]);
+    printf("0x%02X ",rfRConf[i]);
   }
   printf ("\n");
+  if(argc>1){
+    if(strcmp(args[1],"receive")==0){
+      mode = 1;
+      nrfMode(DEVICEWORKMODE_RECEIVE);
+      printf("Receive Mode.\n");
+    }else{
+      mode = 2;
+      printf("Send Mode.\n");
+    }
+  }else{
+    printf("Usage: nrf905 <OPTION>\n OPTION: receive|send \n");
+    return 0;
+  }
+  while(1){
+    if(mode==1){
+      if(dataAvailable()){
+        receive(dataBuf);
+        printf("Data Receive:{");
+        //for(i=1;i<33;i++){
+        //  printf("0x%02x ",dataBuf[i]);
+        //}
+        printf("%s",dataBuf);
+        printf("}\n");
+      }
+    }else{
+      sprintf(dataBuf,"Send Test:%d",index++);
+      printf("Send:{%s},\n",dataBuf);
+      send(txAdd,dataBuf);
+      delay(1000);
+    }
+  }
   return 0;
 }
