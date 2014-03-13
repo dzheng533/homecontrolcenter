0e234b89f04e438196d48ed20186377d4e7925dc
dongzheng <zhengd@wintaoglobal.com>
2014-03-13 23:46:38 +0800

    显示豆瓣电台频道列表

---
+++ douban.fm/js/doubanfm.js
@@ -0,0 +1,24 @@
+//Javascript Object for douban FM
+// Require jQuery library
+//
+//
+var doubanFM = function(){
+	var channelsObj = null;
+
+    loadChannels = function(divObject){
+       channelsObj = $(divObject);
+       var ulChannels = $("<ul></ul>");
+       $.getJSON("http://douban.fm/j/app/radio/channels",function(data){
+       	  for (var i = 0; i < data.channels.length; i++) {
+       	  	var liChannel = $("<li></li>");
+       	  	var butChannel = $("<a channelId='"+data.channels[i].channel_id+"' >" + data.channels[i].name + "</a>");
+       	  	butChannel.bind("click",function(){
+       	  		alert($(this).attr("channelId"));
+       	  	});
+       	  	liChannel.append(butChannel);
+       	  	ulChannels.append(liChannel);
+       	  };
+       	  channelsObj.append(ulChannels);
+       });
+    };
+};
